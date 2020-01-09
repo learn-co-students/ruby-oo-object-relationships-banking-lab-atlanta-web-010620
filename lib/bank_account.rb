8fb19cec3abd023d14300d35dcf246d0c830c05b
tylergreason <tyler.greason@protonmail.com>
2020-01-09 11:56:29 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+    # getters/setters 
+    # attr_accessor :balance
+    attr_reader :name
+    attr_accessor :balance, :status 
 
+    # initialize
+    def initialize(name) 
+        @name = name 
+        @balance = 1000
+        @status = 'open' 
+    end
+
+    # instance methods 
+
+    def deposit(input)
+        @balance+=input
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end  
+    
+    def valid? 
+        if @status == 'open' && @balance > 0 
+            return true 
+        else 
+            return false 
+        end
+    end
+
+    def close_account 
+        @status = 'closed'
+        @status
+    end
 end
